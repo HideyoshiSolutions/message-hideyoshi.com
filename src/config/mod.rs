e4425e057d8978992afa8a2cc944b8310aecec24
Vitor Hideyoshi Nakazone Batista <vitor.h.n.batista@gmail.com>
2024-02-26 01:06:24 -0300

    Re-formats Code and Minor Fixes

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,5 +1,5 @@
 pub mod config_auth;
 pub mod config_email;
-pub mod config_server;
-pub mod config_redis;
 pub mod config_limits;
+pub mod config_redis;
+pub mod config_server;
--- src/depends/depends_email_service.rs
+++ src/depends/depends_email_service.rs
@@ -1,11 +1,8 @@
-use cached::proc_macro::cached;
 use crate::config::config_email::get_config_email;
 use crate::service::email_service::EmailService;
-
+use cached::proc_macro::cached;
 
 #[cached]
 pub fn get_depends_email_service() -> EmailService {
-    EmailService::new(
-        get_config_email()
-    )
+    EmailService::new(get_config_email())
 }
